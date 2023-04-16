c5d7457adb3deb00be7d72a70829405b15990083
seog-jun <shong69@myseneca.ca>
2023-04-15 20:49:36 -0400

    Modified

--- Nooks/useInput.js
+++ Nooks/useInput.js
@@ -2,7 +2,6 @@ export const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
     const val = event.target.value;
-    console.log(val);
     let willUpdate = true;
     if (typeof validator === "function") {
       willUpdate = validator(val);
