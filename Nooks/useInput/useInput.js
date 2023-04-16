dc539426253ff34e52c3903e87db5ab8a45eea86
seog-jun <shong69@myseneca.ca>
2023-04-16 12:46:02 -0400

    Added useClick

--- Nooks/useInput/useInput.js
+++ Nooks/useInput/useInput.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
