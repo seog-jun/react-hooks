1b532b33135e3301e3f065602c6769e4b49c1c3e
seog-jun <shong69@myseneca.ca>
2023-04-26 01:11:35 -0400

    Added useFadeIn hooks

---
+++ Nooks/useFadeIn/useFadeIn.js
@@ -0,0 +1,13 @@
+import { useRef, useEffect } from "react";
+
+export const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
